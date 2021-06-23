d75a52580388e199229cbe1f01070f9999dc631e
Anand Raj <anandallen95@gmail.com>
2021-06-23 15:43:48 +0530

    React Setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BookStore-Shop
 E-commerce Online Book Store Shop using React Js and MongoDB.
+
+
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Welcome To BookStore</h1>
+    </>
+  );
+}
+
+export default App;
