ca5f3d733ef292016266cddce863585f0571af27
Anand Raj <anandallen95@gmail.com>
2021-06-24 01:36:58 +0530

    error handling

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 
 dotenv.config()
 
 connectDB()
-
-
-
 
 
 
@@ -21,6 +19,9 @@ connectDB()
 
 
  app.use('/api/products', productRoutes)
+
+ app.use(notFound)
+ app.use(errorHandler)
 
  
  app.listen(5000, console.log('Server running on port 5000'))
