8aee3894043130584fc31e22f74b80f695bea479
Anand Raj <anandallen95@gmail.com>
2021-06-23 23:43:37 +0530

    Connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import products from './data/products.js'
+import connectDB from './config/db.js'
+
 
 dotenv.config()
+
+connectDB()
+
+
 
 
 
