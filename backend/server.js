8e50d7e02da32ca63d7b1de5ad59f805b5ff54be
Anand Raj <anandallen95@gmail.com>
2021-06-24 01:09:04 +0530

    Fetch products and error handler

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
 
 
 dotenv.config()
@@ -19,14 +19,9 @@ connectDB()
    res.send('API is running...')
  })
 
- app.get('/api/products', (req, res) => {
-   res.json(products)
- })
 
- app.get('/api/products/:id', (req, res) => {
-   const product = products.find((p) => p._id === req.params.id)
-   res.json(product)
- })
+ app.use('/api/products', productRoutes)
+
  
  app.listen(5000, console.log('Server running on port 5000'))
 
