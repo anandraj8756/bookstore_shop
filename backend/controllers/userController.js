980bc65917bb6f583f7ad4d9e677d400af205a4f
Anand Raj <anandallen95@gmail.com>
2021-06-24 04:30:50 +0530

    User auth & generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
  // @desc    Auth user & get token
  // @route   GET /api/users/login
@@ -15,7 +16,7 @@ import User from '../models/userModel.js'
            name: user.name,
            email: user.email,
            isAdmin: user.isAdmin,
-           token: null
+           token: generateToken(user._id),
        }) 
 
    } else{
