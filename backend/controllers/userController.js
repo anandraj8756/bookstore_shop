20abd54ec892fd2c44badffad628b2fb60855319
Anand Raj <anandallen95@gmail.com>
2021-06-24 10:52:02 +0530

    User registration & password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,4 +44,39 @@ import generateToken from '../utils/generateToken.js'
     }
   })
 
- export {authUser, getUserProfile}
+
+// @desc    Register a new user
+ // @route   POST /api/users
+ // @access  Public
+ const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+ 
+
+ export {authUser, getUserProfile, registerUser}
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,16 @@ import bcrypt from 'bcryptjs'
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+
  const User = mongoose.model('User', userSchema)
 
 
